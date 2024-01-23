ce47b57a9465f86d9577b47b8dea93cb4b4dcf96
Scuenco <Scuenco@users.noreply.github.com>
2024-01-23 18:23:56 -0500

    modified package.json, CardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error('There is an Error!!!')
-  }
   return (
     <div>
       {
